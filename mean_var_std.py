8f7e3e0ea0b827b3f5744e97131f2234e41f71dd
bebaran <leandro.filho@sou.inteli.edu.br>
2025-08-19 16:47:04 -0300

    feat: implementação da função no mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,30 @@
 import numpy as np
 
 def calculate(list):
-
-
-
-
-    return calculations
+     
+     if len(list) != 9:
+        raise ValueError("Infelizmente deu erro, a lista tem que ter examente 9 elementos")
+     else :
+         matrix = np.array(list).reshape(3,3)
+     
+         calculations = {
+               'mean' : [matrix.mean(axis=0).tolist(),
+                    matrix.mean(axis=1).tolist(),
+                    matrix.mean().item()],
+               'variance' : [matrix.var(axis=0).tolist(),
+                   matrix.var(axis=1).tolist(),
+                   matrix.var().item()],
+               'standard deviation' : [matrix.std(axis=0).tolist(),
+                                  matrix.std(axis=1).tolist(),
+                                  matrix.std().item()],
+               'max' : [matrix.max(axis=0).tolist(),
+                  matrix.max(axis=1).tolist(),
+                  matrix.max().item()],
+               'min' : [matrix.min(axis=0).tolist(),
+                  matrix.min(axis=1).tolist(),
+                  matrix.min().item()],
+               'sum' : [matrix.sum(axis=0).tolist(),
+                   matrix.sum(axis=1).tolist(),
+                   matrix.sum().item()]
+          }     
+     return calculations
